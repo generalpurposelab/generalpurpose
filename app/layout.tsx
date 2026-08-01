d0bb453cdd14ce06261d786cb536a42f708266d0
Ed Bayes <51138168+ebayes@users.noreply.github.com>
2026-08-01 11:23:26 -0700

    Recreate Dip landing page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,11 @@
-import { Geist, Geist_Mono, Inter } from "next/font/google"
+import type { Metadata } from "next"
 
 import "./globals.css"
-import { ThemeProvider } from "@/components/theme-provider"
-import { cn } from "@/lib/utils";
 
-const inter = Inter({subsets:['latin'],variable:'--font-sans'})
-
-const fontMono = Geist_Mono({
-  subsets: ["latin"],
-  variable: "--font-mono",
-})
+export const metadata: Metadata = {
+  title: "Dip",
+  description: "Dip is a technology lab and holding company.",
+}
 
 export default function RootLayout({
   children,
@@ -17,14 +13,8 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html
-      lang="en"
-      suppressHydrationWarning
-      className={cn("antialiased", fontMono.variable, "font-sans", inter.variable)}
-    >
-      <body>
-        <ThemeProvider>{children}</ThemeProvider>
-      </body>
+    <html lang="en">
+      <body>{children}</body>
     </html>
   )
 }
